d867df6e501efac262ed11d545d81235b655dcc0
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-26 11:46:07 +0600

    added subscription section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+// working with payment quotation section 
+const linuxBtn = document.getElementById("linux")
+const windowsBtn = document.getElementById("windows")
+const linuxContainer = document.querySelector(".linux-container")
+const windowsContainer = document.querySelector(".windows-container")
+linuxBtn.addEventListener("click", function () {
+    // handle button
+    this.classList.add("active")
+    windowsBtn.classList.remove("active")
+    // handle container showing
+    linuxContainer.style.display = "grid"
+    windowsContainer.style.display = "none"
+})
+windowsBtn.addEventListener("click", function () {
+    // handle button
+    this.classList.add("active")
+    linuxBtn.classList.remove("active")
+
+    // handle container showing
+    linuxContainer.style.display = "none"
+    windowsContainer.style.display = "grid"
+})
+
+
 var swiper = new Swiper(".slide-content", {
     spaceBetween: 30,
     pagination: {
