bf85b8bce01e82ab48688654e52a3c66c87ed99a
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-29 21:58:20 +0600

    added favorite section in the linux vps page

--- js/linux.js
+++ js/linux.js
@@ -41,6 +41,32 @@ var swiper = new Swiper(".slide-content", {
         clickable: true,
     },
 });
+// user commennt slider 
+var swiperOfFavSec = new Swiper(".card-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    fade:"true",
+    grabCursor:"true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        }
+    }
+});
 
 
 
